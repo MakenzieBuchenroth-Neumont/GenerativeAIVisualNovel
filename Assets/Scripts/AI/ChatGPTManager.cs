9d9a0b1173e7fce2ca06adf0256b9e702f068096
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2024-11-22 13:19:45 -0700

    save

--- Assets/Scripts/AI/ChatGPTManager.cs
+++ Assets/Scripts/AI/ChatGPTManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using UnityEngine.Events;
 using TMPro;
 using System;
+using static UnityEngine.UIElements.UxmlAttributeDescription;
+using UnityEditor.Overlays;
 
 public class ChatGPTManager : MonoBehaviour
 {
@@ -44,6 +46,7 @@ public class ChatGPTManager : MonoBehaviour
 
             Debug.Log(chatReponse.Content.ToString()); 
             OnResponseEvent.Invoke(this, new onResponseEventArgs { response = chatReponse.Content });
+            save();
         }
 
 	}
@@ -54,18 +57,47 @@ public class ChatGPTManager : MonoBehaviour
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        string askString = "This is a high fantasy world, you will be the dm, if say a line please do Narrator:Image:Line please precede every line with who is speaking. Image is the name of the image of the character, we have ";
-        foreach (var c in CharacterListSO.charactersnames) {
-            askString += "\"" + c + "\", ";
-        }
-        askString += ".If the narrator is speaking say Image is \"Null\" and name as Narrator two examples are \"Narrator:Null:This is the world eldoria.\" and \"Issac:MaleHumanChild:Hi There, I'm isaac. \". start off with describing the setting, then asking about my character. don't use quotation marks at all.";
+        Debug.Log(SaveManager.instance.getString(SaveManager.DataLabel.MESSAGES));
+        if (SaveManager.instance.getString(SaveManager.DataLabel.MESSAGES) == "" || SaveManager.instance.getString(SaveManager.DataLabel.MESSAGES) == null) {
 
-		askChatGpt("This is a high fantasy world, you will be the dm, if say a line please do Narrator:Image:Line please precede every line with who is speaking. Image is the name of the image of the character, we have \"MaleHumanPaladin\", \"MaleHumanRouge\", \"MaleHumanWizard\", \"MaleHumanSorcerer\", \"MaleHumanBard\", \"MaleHumanArtificer\", \"MaleHumanCivilian\", \"FemaleHumanCivilian\", \"MaleHumanChild\", \"FemaleHumanChild\". you have to use one of the given names for image. If the narrator is speaking say Image is \"Null\" and name as Narrator, two examples are \"Narrator:Null:This is the world eldoria.\" and \"Issac:MaleHumanChild:Hi There, I'm isaac. \". start off with describing the setting, then asking about my character. don't use quotation marks at all.");
+            string askString = "This is a high fantasy world, you will be the dm, if say a line please do Narrator:Image:Line please precede every line with who is speaking. Image is the name of the image of the character, we have ";
+            foreach (var c in GameManager.charactersnames) {
+                askString += "\"" + c + "\", ";
+            }
+            askString += "you have to use one of the given names for image.If the narrator is speaking say Image is \"Null\" and name as Narrator, two examples are \"Narrator:Null:This is the world eldoria.\" and \"Issac:MaleHumanChild:Hi There, I'm isaac. \". start off with describing the setting, then asking about my character. don't use quotation marks at all.";
+            askChatGpt(askString);
+        } else {
+            load();
+            OnResponseEvent.Invoke(this, new onResponseEventArgs { response = chatMessages[chatMessages.Count - 1].Content });
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void save() {
+        string SavedString = "";
+        foreach (ChatMessage chatMessage in chatMessages) {
+            SavedString += chatMessage.Content + "/";
+            SavedString += chatMessage.Role + "|";
+        }
+        SaveManager.instance.setString(SaveManager.DataLabel.MESSAGES, SavedString);
+    }
+
+	public void load() {
+        string[] messages = SaveManager.instance.getString(SaveManager.DataLabel.MESSAGES).Split("|");
+        chatMessages = new List<ChatMessage>();
+        foreach (string message in messages) {
+            if (message != "") {
+                string[] messagecontent = message.Split("/");
+                ChatMessage chatMessage = new ChatMessage();
+                chatMessage.Content = messagecontent[0];
+                chatMessage.Role = messagecontent[1];
+                chatMessages.Add(chatMessage);
+            }
+        }
     }
 }
--- Assets/Scripts/Game/SaveManager.cs
+++ Assets/Scripts/Game/SaveManager.cs
@@ -4,33 +4,39 @@ using UnityEngine;
 
 public class SaveManager : MonoBehaviour {
 	public enum DataLabel {
-		TEST_STRING
+		TEST_STRING,
+		MESSAGES
 	}
 
 	public static SaveManager instance {get; private set;}
 
 	//Data
 	private string testString;
+	private string Messages;
 
-	private void Start() {
+	private void Awake() {
 		instance = this;
+		PlayerPrefs.DeleteAll();
 		UpdateData();
 	}
 	private void UpdateData() {
 		testString = PlayerPrefs.GetString(DataLabel.TEST_STRING.ToString(), "");
+		Messages = PlayerPrefs.GetString(DataLabel.MESSAGES.ToString(), "");
 	}
 
-	private void setString(DataLabel data, string newString) {
+	public void setString(DataLabel data, string newString) {
 		PlayerPrefs.SetString(data.ToString(), newString);
 		UpdateData();
 	}
 
 
-	private string getString(DataLabel data) {
+	public string getString(DataLabel data) {
 		switch (data) {
 			default:
 			case DataLabel.TEST_STRING:
 				return testString;
+			case DataLabel.MESSAGES:
+				return Messages;
 		}
 	}
 }
